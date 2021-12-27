c588bbf0b1a6b729875ecd4584bce2a29a052db6
Tanbir Hasan <mdtanbir576@gmail.com>
2021-12-27 22:56:09 +0600

    dashboard design

---
+++ js/script.js
@@ -0,0 +1,24 @@
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+
+const hamburger = document.getElementById("mobile-button");
+var navmenu = document.getElementById("my-task");
+
+hamburger.addEventListener("click", () => {
+  navmenu.classList.toggle("show");
+});
